75f8e678a3d13a69e0239b3c77733b483a438236
smkim <kimsm9822@gmail.com>
2022-09-15 15:33:39 +0900

    config: db, swagger 등 초기 환경설정

---
+++ backend/src/test/java/com/ssafy/hangul/HangulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.hangul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
