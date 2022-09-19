06f08918b10cfe055d67e59efec1493752ee4166
JunSeong <gentlmanjun@naver.com>
2022-09-19 15:56:14 +0900

    feat : 메타마스크 연동 로그인, public address 출력 배준성

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useWeb3React } from '@web3-react/core';
+import { injected } from '../lib/connectors';
+import { isNoEthereumObject } from '../lib/error';
 
 export default function Login() {
+  const { chainId, account, active, activate, deactivate } = useWeb3React();
+
+  const handdleConnect = () => {
+    if (active) {
+      deactivate();
+      return;
+    }
+
+    activate(injected, (error: any) => {
+      if (isNoEthereumObject(error)) {
+        window.open('https://metamask.io/download.html');
+      }
+    });
+  };
+
   return (
     <>
       <div>Login</div>
+      <hr />
+      <div>
+        <p>Account: {account}</p>
+        <p>ChainId: {chainId}</p>
+      </div>
+      <hr />
+      <div>
+        <button type="button" onClick={handdleConnect}>
+          {active ? 'disconnect' : 'connect'}
+        </button>
+      </div>
+
       <div>
         <Link to="/signup">signup</Link>
       </div>
