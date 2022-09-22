168a142225bb4d4db5553239582fac6fffbcfac2
JunSeong <gentlmanjun@naver.com>
2022-09-22 15:16:47 +0900

    feat : 자모음 카드 구현, png 임포트 에러 처리 배준성 LetterCard

--- frontend/src/commons/LetterCard.tsx
+++ frontend/src/commons/LetterCard.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React from 'react';
+import { Card, CardContent, Typography, Button } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import tmpImg from '../img/tmpImg.PNG';
 
-export default function LetterCard(){
-  
+interface LetterCardType {
+  description: String;
+  title: String;
+  letter: String;
+  quantity: Number;
+}
+
+export default function LetterCard({ description, title, letter, quantity }: LetterCardType) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      {/* 이미지 링크 수정필요함 */}
+      <CardMedia component="img" height="150" image={tmpImg} alt="green iguana" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Quantity : {`${quantity}`}
+          <br />
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
