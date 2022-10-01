63647486a1019287888bafc1f481b17d19b904c6
smkim <kimsm9822@gmail.com>
2022-10-01 22:13:29 +0900

    feat: 구매 로직 초안 추가

---
+++ frontend/src/components/NFTDetail/BuyFunction.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import Web3 from 'web3';
+import SsafyToken from '../../json/SsafyToken.json';
+import SaleFactory from '../../json/SaleFactory.json';
+import Sale from '../../json/Sale.json';
+
+async function BuyFunction(price, tokenId, nftAddress) {
+    const tokenCA = process.env.REACT_APP_TOKEN_CONTRACT_ADDRESS;
+    const saleFactoryCA = process.env.REACT_APP_SALE_CONTRACT_ADDRESS;
+
+    const web3 = new Web3(window.ethereum);
+    const tokenABI = SsafyToken.abi;
+    const tokenContract = new web3.eth.Contract(tokenABI, tokenCA);
+    const saleFactoryABI = SaleFactory.abi;
+    const saleFactoryContract = new web3.eth.Contract(saleFactoryABI, saleFactoryCA);
+    const saleABI = Sale.abi;
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const userAddress = accounts[0]; // 사용자의 지갑 주소
+
+    const getSaleContract = saleAddress => {
+        const SaleContract = new web3.eth.Contract(
+            saleABI,
+            saleAddress
+        )
+        return SaleContract;
+    };
+
+    // sale컨트랙트 주소 받아서 생성
+    const saleCA = await saleFactoryContract.methods
+        .getSaleContractAddress(tokenId)
+        .call();
+    const saleContract = getSaleContract(saleCA);
+
+    // sale컨트랙트로 erc20토큰 전송권한 허용
+    await tokenContract.methods
+        .approve(saleCA, price)
+        .send({ userAddress });
+
+    // 구매 요청
+    await saleContract.methods.purchase(price).send({ userAddress });
+
+    // 판매 기록 API 호출
+    // api/sale  post: {nftId, buyerWalletAddress, saleContractAddress}
+    axios.post("http://localhost:8081/api/sale", { nftAddress, buyerWalletAddress: userAddress, saleContractAddress: saleCA })
+}
