945455741ac0ebdd6850b886b47ba1790ec1b990
JunSeong <gentlmanjun@naver.com>
2022-10-01 12:53:52 +0900

    feat : 유저 페이지 이동 시 유저스테이트 업데이트 배준성

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import UserInfo from '../components/userPage/UserInfo';
 import UserCardList from '../components/userPage/UserCardList';
+import { fetchUser } from '../_slice/UserSlice';
 
 export default function UserPage() {
   const params = useParams();
   const userAddress = params.walletAddress;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUser(userAddress));
+  });
 
   return (
     <>
