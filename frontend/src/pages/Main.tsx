9a894495df6813a6218b9b248de6db121733ec79
박민호 <tkdfhralsgh@naver.com>
2022-10-04 00:58:08 +0900

    feat: 랜딩페이지 제작중 박민호 Area[Information/TutorialInfo].tsx Information.tsx MainLand.tsx Main.tsx

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import MainLand from '../components/main/MainLand';
+import AreaTutorialInfo from '../components/main/AreaTutorialInfo';
 
 export default function Main() {
   return (
     <div>
-      <div>Main</div>
+      <Grid container style={{width:window.innerHeight}}>
+        <Grid item xs={6}><MainLand /></Grid>
+        <Grid item xs={6}><MainLand /></Grid>
+      </Grid>
+      <AreaTutorialInfo />
       <div>
         <Link to="/composehangul">create</Link>
       </div>
