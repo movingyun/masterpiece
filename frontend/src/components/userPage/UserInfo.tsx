3226e734b531fa86ac0f1e3a2179dc38aac057d3
JunSeong <gentlmanjun@naver.com>
2022-10-05 11:03:30 +0900

    feat : 유저 정보 변경

--- frontend/src/components/userPage/UserInfo.tsx
+++ frontend/src/components/userPage/UserInfo.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { Card, CardContent, Tooltip } from '@mui/material';
+import { Card, CardContent, Tooltip, Button, TextField, FormControl } from '@mui/material';
 import { Instance } from '@popperjs/core';
-import { fetchUser } from '../../_slice/UserSlice';
+import { editUser, fetchUser } from '../../_slice/UserSlice';
 import useCopyClipBoard from '../../_hook/useCopyClipBoard';
 import NFTPreview from '../../commons/NFTPreview';
+
+const ALLOW_FILE_EXTENSION = 'jpg,jpeg,png';
+const FILE_SIZE_MAX_LIMIT = 5 * 1024 * 1024;
 
 const StyledCardList = styled.div`
   display: flex;
@@ -13,13 +16,31 @@ const StyledCardList = styled.div`
     margin: 2px;
   }
 `;
+const StyledWord = styled.div`
+  margin: 20px;
+  font-size: 20px;
+  font-weight: 500;
+`;
+const StyledName = styled.h1`
+  margin-bottom: 20px;
+  margin-top: 0px;
+`;
+
+const StyledBtn = styled.div`
+  text-align: right;
+  margin-top: 20px;
+  margin-right: 20px;
+`;
 
 const StyledCopyWallet = styled.div`
+  margin: 20px;
+  font-size: 20px;
+  font-weight: 500;
   cursor: pointer;
 `;
 
 export interface UserInfoType {
-  walletAddress: String;
+  walletAddress: string;
 }
 export default function UserInfo({ walletAddress }: UserInfoType) {
   const dispatch = useDispatch();
@@ -29,6 +50,79 @@ export default function UserInfo({ walletAddress }: UserInfoType) {
   }, []);
 
   const searchedUser = useSelector((state: any) => state.user.searchedUser);
+  const currentUser = useSelector((state: any) => state.user.currentUser);
+
+  // 정보 수정
+  const [isEdit, setIsEdit] = useState(false);
+  const [nickname, setNickname] = useState(searchedUser.nickname);
+  const [message, setMessage] = useState(searchedUser.message);
+  const [file, setFile] = useState<any>();
+  const [imageSrc, setImageSrc] = useState('');
+
+  // 파일 업로드
+  const fileUploadValidHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.currentTarget;
+    const files = (target.files as FileList)[0];
+
+    if (files === undefined) {
+      return;
+    }
+
+    // 파일 확장자 체크
+    if (!fileExtensionValid(files)) {
+      target.value = '';
+      alert(`업로드 가능한 확장자가 아닙니다. [가능한 확장자 : ${ALLOW_FILE_EXTENSION}]`);
+      return;
+    }
+
+    // 파일 용량 체크
+    if (files.size > FILE_SIZE_MAX_LIMIT) {
+      target.value = '';
+      alert('업로드 가능한 최대 용량은 5MB입니다. ');
+      return;
+    }
+
+    // validation을 정상적으로 통과한 File
+    setFile(files);
+    encodeFileToBase64(files);
+    console.log(files);
+  };
+
+  const fileExtensionValid = ({ name }: { name: string }): boolean => {
+    // 파일 확장자
+    const extension = removeFileName(name);
+
+    if (!(ALLOW_FILE_EXTENSION.indexOf(extension) > -1) || extension === '') {
+      return false;
+    }
+    return true;
+  };
+
+  const removeFileName = (originalFileName: string): string => {
+    const lastIndex = originalFileName.lastIndexOf('.');
+
+    if (lastIndex < 0) {
+      return '';
+    }
+
+    return originalFileName.substring(lastIndex + 1).toLowerCase();
+  };
+
+  // 파일 미리보기
+  const encodeFileToBase64 = (fileBlob: any) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(fileBlob);
+
+    return new Promise(resolve => {
+      reader.onload = () => {
+        const resultA = `${reader.result}`;
+        setImageSrc(resultA);
+
+        resolve(imageSrc);
+      };
+    });
+  };
 
   // 클릭 시 지갑주소 복사
 
@@ -54,41 +148,111 @@ export default function UserInfo({ walletAddress }: UserInfoType) {
     }
   };
 
+  const editHander = async () => {
+    if (isEdit) {
+      const formData = new FormData();
+      formData.append('wallet_address', walletAddress);
+      formData.append('profileImage', file);
+      formData.append('nickname', nickname);
+      formData.append('message', message);
+
+      await dispatch(editUser(formData));
+      setIsEdit(false);
+      dispatch(fetchUser(walletAddress));
+    } else {
+      setIsEdit(true);
+    }
+  };
+
+  const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+  };
+  const onChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
   return (
     <StyledCardList>
-      <Card sx={{ width: '30%', minWidth: 200 }}>
-        <NFTPreview url={`${searchedUser.profileImage}`} />
-      </Card>
-      <Card sx={{ width: '70%' }}>
-        <CardContent>
-          <h1>{searchedUser.nickname}</h1>
-          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
-          <Tooltip
-            title="Copy"
-            arrow
-            placement="top"
-            PopperProps={{
-              popperRef,
-              anchorEl: {
-                getBoundingClientRect: () =>
-                  new DOMRect(positionRef.current.x, areaRef.current!.getBoundingClientRect().y, 0, 0),
-              },
-            }}>
-            <StyledCopyWallet
-              onClick={() => {
-                handleCopy(`${searchedUser.wallet_address}`);
-              }}
-              tabIndex={0}
-              role="button"
-              ref={areaRef}
-              onMouseMove={handleMouseMove}>
-              {searchedUser.wallet_address}
-            </StyledCopyWallet>
-          </Tooltip>
-          <div>Joined : {searchedUser.joinDate}</div>
-          <div>{searchedUser.message}</div>
-        </CardContent>
-      </Card>
+      {isEdit ? (
+        // 정보 수정
+        <>
+          <Card sx={{ width: '30%', minWidth: 200 }}>
+            <h1>프로필 변경</h1>
+            <input type="file" onChange={fileUploadValidHandler} />
+            {file ? imageSrc && <NFTPreview url={imageSrc} /> : <NFTPreview url={`${searchedUser.profileImage}`} />}
+          </Card>
+          <Card sx={{ width: '70%' }}>
+            <StyledBtn>
+              {currentUser.walletAddress === searchedUser.walletAddress ? (
+                <Button onClick={editHander}>Done</Button>
+              ) : null}
+            </StyledBtn>
+            <CardContent>
+              <TextField
+                fullWidth
+                required
+                label="Nickname"
+                variant="outlined"
+                defaultValue={searchedUser.nickname}
+                value={nickname}
+                onChange={onChangeNickname}
+              />
+              <StyledWord>Wallet Address : {searchedUser.wallet_address} </StyledWord>
+              <StyledWord>Joined : {searchedUser.joinDate}</StyledWord>
+              <TextField
+                fullWidth
+                label="Message"
+                variant="outlined"
+                defaultValue={searchedUser.message}
+                value={message}
+                onChange={onChangeMessage}
+              />
+            </CardContent>
+          </Card>
+        </>
+      ) : (
+        // 정보 출력
+        <>
+          <Card sx={{ width: '30%', minWidth: 200 }}>
+            <NFTPreview url={`${searchedUser.profileImage}`} />
+          </Card>
+          <Card sx={{ width: '70%' }}>
+            <StyledBtn>
+              {currentUser.walletAddress === searchedUser.walletAddress ? (
+                <Button onClick={editHander}>Edit</Button>
+              ) : null}
+            </StyledBtn>
+            <CardContent>
+              <StyledName>{searchedUser.nickname}</StyledName>
+              {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+              <Tooltip
+                title="Copy"
+                arrow
+                placement="top"
+                PopperProps={{
+                  popperRef,
+                  anchorEl: {
+                    getBoundingClientRect: () =>
+                      new DOMRect(positionRef.current.x, areaRef.current!.getBoundingClientRect().y, 0, 0),
+                  },
+                }}>
+                <StyledCopyWallet
+                  onClick={() => {
+                    handleCopy(`${searchedUser.wallet_address}`);
+                  }}
+                  tabIndex={0}
+                  role="button"
+                  ref={areaRef}
+                  onMouseMove={handleMouseMove}>
+                  {searchedUser.wallet_address}
+                </StyledCopyWallet>
+              </Tooltip>
+              <StyledWord>Joined : {searchedUser.joinDate}</StyledWord>
+              <StyledWord>{searchedUser.message}</StyledWord>
+            </CardContent>
+          </Card>
+        </>
+      )}
     </StyledCardList>
   );
 }
